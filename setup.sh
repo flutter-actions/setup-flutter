cbf2b87ee080513b2eab300a3b1ce2d5711c2eca
Socheat Sok <alex@socheat.net>
2022-06-17 17:22:54 +0700

    feat(core): re-write action to support cache and Apple Silicon

--- setup.sh
+++ setup.sh
@@ -1,54 +1,81 @@
 #!/bin/bash
 
-# Parse SDK and version args
-CHANNEL="${1:-stable}"
-VERSION="${3:-3.0.2}"
+# Options
+ARCH=$(echo "${RUNNER_ARCH:-x64}" | awk '{print tolower($0)}')
+OS=$(echo "${RUNNER_OS:-macOS}" | awk '{print tolower($0)}')
 
-# Parse OS Environment
-OS="${2:-Linux}"
-OS=$(echo "$OS" | awk '{print tolower($0)}')
+# Args
+FLUTTER_CHANNEL=${1:-stable}
+FLUTTER_VERSION=${2:-3.0.2}
+FLUTTER_OS=$OS
+FLUTTER_ARCH=""
 
 # OS archive file extension
 EXT="zip"
 if [[ $OS == linux ]]
 then
-  EXT="tar.xz"
+	EXT="tar.xz"
+fi
+
+# Apple Intel or Apple Silicon
+if [[ $OS == macos ]]
+then
+	if [[ $ARCH == 'arm64' ]]
+	then
+		FLUTTER_ARCH="_$ARCH"
+	fi
 fi
 
 # Flutter runner tool cache
-FLUTTER_RUNNER_TOOL_CACHE="${RUNNER_TOOL_CACHE}/${VERSION}-${CHANNEL}"
+# path: "${{ runner.tool_cache }}/flutter-${{ runner.os }}-${{ inputs.version }}-${{ runner.arch }}"
+# key: flutter-action-setup-flutter-${{ runner.os }}-${{ inputs.version }}-${{ runner.arch }}
+FLUTTER_RUNNER_TOOL_CACHE="${RUNNER_TOOL_CACHE}/flutter-${RUNNER_OS}-${FLUTTER_VERSION}-${RUNNER_ARCH}"
 
 # Check if Flutter SDK already exists
 # Otherwise download and install
+# https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.0.2-stable.zip
+FLUTTER_RELEASE_URL="https://storage.googleapis.com/flutter_infra_release/releases"
+
+
 if [ ! -d "${FLUTTER_RUNNER_TOOL_CACHE}" ]; then
-  echo "Installing Flutter SDK version \"${VERSION}\" from the ${CHANNEL} channel on ${OS}"
+	echo "Installing Flutter SDK version \"${FLUTTER_VERSION}\" from the ${FLUTTER_CHANNEL} channel on ${FLUTTER_OS}"
 
-  # Calculate download Url. Based on:
-  # https://flutter.dev/docs/development/tools/sdk/releases
-  PREFIX="https://storage.googleapis.com/flutter_infra_release/releases"
-  BUILD="flutter_${OS}_${VERSION}-${CHANNEL}.${EXT}"
+	# Linux
+	# /stable    /linux/     flutter_linux_2.10.2-stable.tar.xz
+	# /beta      /linux/     flutter_linux_3.1.0-9.0.pre-beta.tar.xz
 
-  URL="${PREFIX}/${CHANNEL}/${OS}/${BUILD}"
-  echo "Downloading ${URL}..."
+	# macOS
+	# /stable    /macos/     flutter_macos_3.0.2-stable.zip
+	# /stable    /macos/     flutter_macos_arm64_3.0.2-stable.zip
+	# /beta      /macos/     flutter_macos_arm64_3.1.0-9.0.pre-beta.zip
+	# /beta      /macos/     flutter_macos_3.1.0-9.0.pre-beta.zip
 
-  # Download installation archive
-  curl --connect-timeout 15 --retry 5 "$URL" > "/tmp/${BUILD}"
+	# Windows
+	# /stable    /windows/   flutter_windows_3.0.2-stable.zip
+	# /beta      /windows/   flutter_windows_3.1.0-9.0.pre-beta.zip
+	FLUTTER_BUILD="flutter_${FLUTTER_OS}${FLUTTER_ARCH}_${FLUTTER_VERSION}-${FLUTTER_CHANNEL}.${EXT}"
+	FLUTTER_DOWNLOAD_URL="${FLUTTER_RELEASE_URL}/${FLUTTER_CHANNEL}/${FLUTTER_OS}/${FLUTTER_BUILD}"
 
-  # Prepare tool cache folder
-  mkdir -p "${FLUTTER_RUNNER_TOOL_CACHE}"
+	echo "Downloading ${FLUTTER_DOWNLOAD_URL}"
 
-  # Extracting installation archive
-  if [[ $OS == linux ]]
-  then
-    tar -C "${FLUTTER_RUNNER_TOOL_CACHE}" -xf "/tmp/${BUILD}" > /dev/null
-  else
-    unzip "/tmp/${BUILD}" -d "${FLUTTER_RUNNER_TOOL_CACHE}" > /dev/null
-  fi
+	# Download installation archive
+	curl --connect-timeout 15 --retry 5 "$FLUTTER_DOWNLOAD_URL" > "/tmp/${FLUTTER_BUILD}"
 
-  if [ $? -ne 0 ]; then
-    echo -e "::error::Download failed! Please check passed arguments."
-    exit 1
-  fi
+	# Prepare tool cache folder
+	mkdir -p "${FLUTTER_RUNNER_TOOL_CACHE}"
+
+	# Extracting installation archive
+	if [[ $OS == linux ]]
+	then
+		tar -C "${FLUTTER_RUNNER_TOOL_CACHE}" -xf "/tmp/${BUILD}" > /dev/null
+	else
+		unzip "/tmp/${BUILD}" -d "${FLUTTER_RUNNER_TOOL_CACHE}" > /dev/null
+	fi
+
+	if [ $? -ne 0 ]; then
+		echo -e "::error::Download failed! Please check passed arguments."
+		exit 1
+	fi
 fi
 
 # Configure pub to use a fixed location.
