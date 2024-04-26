f89d1e4822b340aa3ff0a9bb970d14494152e14f
Socheat Sok <alex@socheat.net>
2024-04-27 01:19:15 +0700

    Add action_test.sh script

---
+++ action_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export ACTION_TEST_DIR="$(pwd)/.test"
+
+# Runner environment variables
+export RUNNER_TOOL_CACHE="$ACTION_TEST_DIR/tool_cache"
+export RUNNER_TEMP="$ACTION_TEST_DIR/temp"
+export RUNNER_ARCH=$(uname -m)
+export RUNNER_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+if [ "$RUNNER_OS" = "darwin" ]; then
+    export RUNNER_OS="macos"
+fi
+
+# GitHub Context
+export GITHUB_ENV="$ACTION_TEST_DIR/.env"
+export GITHUB_PATH="$ACTION_TEST_DIR/.path"
+
+mkdir -p "$RUNNER_TOOL_CACHE" "$RUNNER_TEMP"
+exec ./action.sh "$@"
